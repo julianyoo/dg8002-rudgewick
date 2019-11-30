a4d69a2b774474058661aa94a149013e913f9da4
julianyoo <julian.yoo@gmail.com>
2019-11-29 23:30:26 -0500

    contact form

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
 /* CONTACT FORM LOGIC */
 
-function validate() {
+function validate_contact() {
     var getType = document.getElementById("formType");
     var selectedValue = getType.options[getType.selectedIndex].value;
     if (selectedValue == "general") {
-        alert("You selected general");
+        document.getElementById("general").style.display = "block";
+        document.getElementById("park").style.display = "none";
+        document.getElementById("vendor").style.display = "none";
+        document.getElementById("camping").style.display = "none";
     } else if (selectedValue == "park") {
-        alert("You selected Park!");
+        document.getElementById("park").style.display = "block";
+        document.getElementById("general").style.display = "none";
+        document.getElementById("vendor").style.display = "none";
+        document.getElementById("camping").style.display = "none";
     } else if (selectedValue == "vendor") {
-        alert("You selected vendor!");
+        document.getElementById("vendor").style.display = "block";
+        document.getElementById("general").style.display = "none";
+        document.getElementById("park").style.display = "none";
+        document.getElementById("camping").style.display = "none";
     } else if (selectedValue == "camping") {
-        alert("You selected camping!");
+        document.getElementById("camping").style.display = "block";
+        document.getElementById("general").style.display = "none";
+        document.getElementById("park").style.display = "none";
+        document.getElementById("vendor").style.display = "none";
     }
 }

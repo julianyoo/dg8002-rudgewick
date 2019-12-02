3c8e7509cc828af7532613fbbefec820e401c297
julianyoo <julian.yoo@gmail.com>
2019-12-01 19:17:50 -0500

    purchaseform

--- main.js
+++ main.js
@@ -37,8 +37,6 @@ function thankYou() {
 
 /* TICKET PURCHASE LOGIC */
 
-
-
 var adultTotal = 0
 var childTotal = 0
 var foodTotal = 0
@@ -47,6 +45,7 @@ var multiplier = 0
 var days = [0,0,0]
 var total = 0
 
+/* Ticketing buttons */
 
 function addAdult() {
     adultTotal++
@@ -147,7 +146,8 @@ function grandTotal() {
 
     total = adultCost + childCost + foodCost + rideCost
 
-    return document.getElementById("grand_total").innerHTML = "£" + total
+    return document.getElementById("grand_total").innerHTML = "£" + total,
+    document.getElementById("contact_total").innerHTML = "£" + total
 }
 
 function clearAll() {
@@ -170,5 +170,42 @@ function clearAll() {
         document.getElementById("day3").checked = false;
     } else {
 
+    }
+}
+
+function next() {
+    if (total > 0 ) {
+    document.getElementById("calculator").style.display = "none"
+    document.getElementById("contact").style.display = "block"
+    document.getElementById("warning").innerHTML = ""
+} else {
+    document.getElementById("warning").innerHTML = "You must have something in your basket in order to continue with the transaction."
+}
+}
+
+function back() {
+    document.getElementById("calculator").style.display = "block"
+    document.getElementById("contact").style.display = "none"
+}
+
+var isValid = false;
+
+function submit() {
+    if (document.getElementById("fname").value === "") {
+        isValid = false;
+        alert('form is bad')
+    } else {
+        isValid = true;
+    }
+
+    if (document.getElementById("lname").value === "") {
+        isValid = false;
+        alert('form is bad')
+    } else {
+        isValid = true;
+    }
+
+    if (isValid === true) {
+        alert("form is gud")
     }
 }
